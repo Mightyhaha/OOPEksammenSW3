7139dd9db7404374ddc601c38ded94af2769ed20
Mightyhaha <74659365+Mightyhaha@users.noreply.github.com>
2021-11-05 13:27:35 +0100

    Added gitignore and started project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OOPEksammenSW3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
