62492efd4d3a3248c19668c49334bb8b5180a65f
Mightyhaha <74659365+Mightyhaha@users.noreply.github.com>
2021-11-05 13:53:53 +0100

    Beginning work on simple classes for user and product

---
+++ Product.cs
@@ -0,0 +1,17 @@
+namespace OOPEksammenSW3
+{
+    class Product
+    {
+
+        int id;
+        string name;
+        int price;
+
+
+
+        public void CanBeBoughtOnCredit()
+        {
+
+        }
+    }
+}
---
+++ User.cs
@@ -0,0 +1,13 @@
+namespace OOPEksammenSW3
+{
+    public class User
+    {
+        int id;
+        string Firstname;
+        string Lastname;
+        string username;
+        public string email;
+        int balance;
+
+    }
+}
